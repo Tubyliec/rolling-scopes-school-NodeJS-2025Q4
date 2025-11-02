c6250668d97280932fca25b5164a6efeebf9efc4
tubyliec <antikruk@vivaldi.net>
2025-11-02 15:28:58 +0300

    feat: add exit processing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import readline from 'readline';
+
 import { parseUserName } from './modules/parseUserName.js';
 
 
@@ -5,3 +7,25 @@ const args = process.argv.slice(2);
 const userName = parseUserName(args);
 
 console.log(`Welcome to the File Manager, ${userName}!`);
+
+const commandLine = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+
+commandLine.on('line', async (input) => {
+    const trimmedInput = input.trim();
+
+    switch (trimmedInput) {
+        case 'exit':
+            console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+            process.exit(0);
+    }
+});
+
+
+commandLine.on('close', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit(0);
+});
