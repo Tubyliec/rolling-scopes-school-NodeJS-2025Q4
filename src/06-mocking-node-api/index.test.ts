ce56004394fd68f01ea3c03a4ebca7b5495bb628
tubyliec <antikruk@vivaldi.net>
2025-11-09 21:33:28 +0300

    feat: add last solutions

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,5 +1,5 @@
-// Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import path from 'node:path';
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -11,11 +11,25 @@ describe('doStuffByTimeout', () => {
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    jest.spyOn(global, 'setTimeout');
+    const callback = jest.fn();
+    const timeout = 3000;
+
+    doStuffByTimeout(callback, timeout);
+
+    expect(setTimeout).toHaveBeenCalledWith(callback, timeout);
+    expect(callback).toHaveBeenCalledTimes(0);
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    jest.spyOn(global, 'setTimeout');
+    const callback = jest.fn();
+    const timeout = 3000;
+
+    doStuffByTimeout(callback, timeout);
+    jest.runAllTimers();
+
+    expect(callback).toHaveBeenCalledTimes(1);
   });
 });
 
@@ -29,24 +43,48 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    jest.spyOn(global, 'setInterval');
+    const callback = jest.fn();
+    const interval = 3000;
+
+    doStuffByInterval(callback, interval);
+
+    expect(setInterval).toHaveBeenCalledWith(callback, interval);
+    expect(callback).toHaveBeenCalledTimes(0);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    jest.spyOn(global, 'setInterval');
+    const callback = jest.fn();
+    const interval = 2000;
+
+    doStuffByInterval(callback, interval);
+
+    jest.advanceTimersByTime(interval);
+    expect(callback).toHaveBeenCalledTimes(1);
+    jest.advanceTimersByTime(interval);
+    expect(callback).toHaveBeenCalledTimes(2);
+    jest.advanceTimersByTime(interval);
+    expect(callback).toHaveBeenCalledTimes(3);
+    jest.advanceTimersByTime(interval);
+    expect(callback).toHaveBeenCalledTimes(4);
   });
 });
 
 describe('readFileAsynchronously', () => {
   test('should call join with pathToFile', async () => {
-    // Write your test here
+    jest.spyOn(path, 'join');
+    await readFileAsynchronously('file.txt');
+    expect(path.join).toHaveBeenCalledTimes(1);
   });
 
   test('should return null if file does not exist', async () => {
-    // Write your test here
+    const result = await readFileAsynchronously('file.ts');
+    expect(result).toEqual(null);
   });
 
   test('should return file content if file exists', async () => {
-    // Write your test here
+    const result = await readFileAsynchronously('index.ts');
+    expect(result).not.toEqual(null);
   });
 });
--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,39 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+jest.mock('lodash', () => ({
+  throttle: (fn: unknown) => fn,
+}));
+
+const mockData = { id: 1, title: 'Test' };
+const path = '/test';
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockValue = jest.fn().mockResolvedValue({ data: mockData });
+    const mockAxiosInstance = { get: mockValue };
+    mockedAxios.create.mockReturnValue(mockAxiosInstance as any);
+    await throttledGetDataFromApi(path);
+    expect(mockedAxios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockValue = jest.fn().mockResolvedValue({ data: mockData });
+    const mockAxiosInstance = { get: mockValue };
+    mockedAxios.create.mockReturnValue(mockAxiosInstance as any);
+    await throttledGetDataFromApi(path);
+    expect(mockValue).toHaveBeenCalledWith(path);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockValue = jest.fn().mockResolvedValue({ data: mockData });
+    const mockAxiosInstance = { get: mockValue };
+    mockedAxios.create.mockReturnValue(mockAxiosInstance as any);
+    const result = await throttledGetDataFromApi(path);
+    expect(result).toEqual(mockData);
   });
 });
