2ca3ef438b0c36389b2ad55131bd47b4fdd43d2e
tubyliec <antikruk@vivaldi.net>
2025-11-25 22:31:17 +0300

    feat: add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './modules/database/database.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: ['config/.env'] })],
+  imports: [ConfigModule.forRoot({ envFilePath: ['config/.env'] }), DatabaseModule, UserModule],
   controllers: [],
   providers: [],
 })
