cd16786316e33f93dd2beca2caad702c5b051126
tubyliec <antikruk@vivaldi.net>
2025-11-25 22:01:11 +0300

    feat: add config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const configService = app.get(ConfigService);
+  const port: string = configService.get('PORT');
+  await app.listen(port, () => {
+    process.stdout.write(`Server started on port ${port}`);
+  });
 }
-bootstrap();
+bootstrap().catch((error: Error) => {
+  process.stderr.write(error.message);
+});
