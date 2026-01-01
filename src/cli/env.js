1a4d0d87a942c4d65330abf4f4de9e8eddb23a93
tubyliec <antikruk@vivaldi.net>
2026-01-02 01:31:44 +0300

    refactor: delete unnecessary files

--- src/cli/env.js
+++
@@ -1,8 +0,0 @@
-const parseEnv = () => {
-    const filteredVariables = Object.entries(process.env).filter(([key]) => key.includes("RSS_"));
-    for (let variable of filteredVariables) {
-        console.log(`RSS_name${filteredVariables.indexOf(variable) + 1}=${variable[1]}`);
-    }
-};
-
-parseEnv();
